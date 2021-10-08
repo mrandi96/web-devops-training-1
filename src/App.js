f74eced0a9db86997a3796cd930ab34ed90c3a88
Muhammad Randi Pratama <randi@code.id>
2021-10-09 03:23:20 +0700

    modify workflow script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This file has been edited to check if the deploy script has run successfully.
         </p>
         <a
           className="App-link"
