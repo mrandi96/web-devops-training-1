ff7ad1db66f20247cd9ef5bd1428a66f8a418006
Muhammad Randi Pratama <randi@code.id>
2021-10-09 03:25:33 +0700

    commit #2 to check cd script activation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This file has been edited to check if the deploy script has run successfully.
+          This file has been edited 2 times to check if the deploy script has run successfully.
         </p>
         <a
           className="App-link"
