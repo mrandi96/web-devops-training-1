2f67c73c21785d4a38629485ae4af2d83fa7cbb3
Muhammad Randi Pratama <randi@code.id>
2021-10-11 18:14:40 +0700

    add more test script

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('successfully edited', () => {
+  render(<App />);
+  const fileEdited = screen.getByText(/been edited/i);
+  expect(fileEdited).toBeInTheDocument();
+});
+
+test('successfully deployed', () => {
+  render(<App />);
+  const deploySuccess = screen.getByText(/run successfully/i);
+  expect(deploySuccess).toBeInTheDocument();
+});
